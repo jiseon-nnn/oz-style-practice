a49525850a42d924c39a05f9e86e62d1cf1e46d0
jiseon_an <ajsajs1031@gmail.com>
2024-10-24 00:20:40 +0900

    SCSS -> styled-components 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import "./App.scss";
+// import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled from "styled-components";
+import { flexMixin } from "./styled/styled";
+
 
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <StyledSection>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </StyledSection>
     </main>
   );
 }
 
+const StyledSection = styled.section`
+  ${flexMixin(undefined, 'center', 'center', '20px', 'wrap')}
+  padding: 20px 40px;
+`
 export default App;
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,46 @@
+import styled from "styled-components";
+import { flexMixin, fontMixin, gray, tag_color } from "../styled/styled";
+
 export default function Content({ content }) {
   return (
-    <div className="content-container">
+    <StyledDiv>
       <img src={content.img} alt={content.title} />
       <span>모집중</span>
       <div>{content.title}</div>
       <p>{content.subtitle}</p>
-    </div>
+    </StyledDiv>
   );
 }
+
+const StyledDiv = styled.div`
+  ${flexMixin('column', undefined, 'flex-start', '5px')}
+
+  img {
+    width: 300px;
+    border-radius: 10px;
+    margin-bottom: 3px;
+    min-height: 225px;
+  }
+
+  span {
+    ${fontMixin('12px')}
+    color: ${tag_color};
+    border: 1px solid ${tag_color};
+    padding: 4px 5px;
+    border-radius: 3px;
+  }
+  
+  div {
+    ${fontMixin('18px', '600')}
+  }
+
+  p {
+    ${fontMixin('12px')}
+    color: ${gray};
+  }
+
+`
+
+
+
+  
